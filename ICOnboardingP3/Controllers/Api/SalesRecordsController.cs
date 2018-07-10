b9a86f5ecd20729e72c9bd92eb01e010a549d891
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-07-10 10:17:51 +1000

    Finished all views

---
+++ ICOnboardingP3/Controllers/Api/SalesRecordsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ICOnboardingP3.Models;
+
+namespace ICOnboardingP3.Controllers.Api
+{
+    public class SalesRecordsController : ApiController
+    {
+        private readonly OnboardingP3Entities db;
+
+        public SalesRecordsController()
+        {
+            db = new OnboardingP3Entities();
+        }
+
+        //GET api/salesrecords
+        public IEnumerable<ProductSold> GetSalesRecords()
+        {
+            var productSolds = db.ProductSolds;
+
+            return productSolds;
+        }
+
+        //GET api/salesrecords/id
+        public ProductSold GetSalesRecord(int id)
+        {
+            var productSold = db.ProductSolds.SingleOrDefault(ps => ps.Id == id);
+
+            if (productSold == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return productSold;
+        }
+
+        //POST api/salesrecords
+        [HttpPost]
+        public ProductSold CreateSalesRecord(ProductSold productSold)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            db.ProductSolds.Add(productSold);
+            db.SaveChanges();
+
+            return productSold;
+        }
+
+        //PUT /api/salesrecords/id
+        [HttpPut]
+        public void UpdateSalesRecord(int id, ProductSold productSold)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var productSoldInDb = db.ProductSolds.SingleOrDefault(ps => ps.Id == id);
+
+            if (productSoldInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            productSoldInDb.DateSold = productSold.DateSold;
+            productSoldInDb.CustomerId = productSold.CustomerId;
+            productSoldInDb.ProductId = productSold.ProductId;
+            productSoldInDb.StoreId = productSold.StoreId;
+
+            db.SaveChanges();
+        }
+
+        //DELETE /api/salesrecords/id
+        [HttpDelete]
+        public void DeleteSalesRecord(int id)
+        {
+            var productSoldInDb = db.ProductSolds.SingleOrDefault(ps => ps.Id == id);
+
+            if (productSoldInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            db.ProductSolds.Remove(productSoldInDb);
+            db.SaveChanges();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ ICOnboardingP3/Controllers/SalesRecordsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICOnboardingP3.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        // GET: SalesRecords
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
